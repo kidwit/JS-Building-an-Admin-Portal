35435709383dff4fca49acef4e5fd05d2a8abcb1
Kidwit <toj817@gmail.com>
2024-09-12 17:33:01 -0700

    Built portal

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,15 @@
 
 // Your Code Here
+
+let response = await fetch('http://localhost:9001/updateBook', {
+    method: "PATCH",
+    headers: {
+        'Content Type' : 'application/json'
+    },
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }),
+});
+let updatedBook = await response.json();
+console.log(updatedBook)
--- public/index.js
+++ public/index.js
@@ -19,10 +19,40 @@ function renderBook(book) {
                     <h5 class="card-title">${book.title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
                     <p class="card-text">${book.description}</p>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Enter text" id="quantityInput-${book.id}">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" onclick="updateQuantity(${book.id})">Update Quantity</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     `
-}
-
-main()
+  }
+  
+  async function updateQuantity(bookId) {
+    const quantityInput = document.getElementById(`quantityInput-${bookId}`);
+    const newQuantity = quantityInput.value;
+  
+  
+    const response = await fetch(`http://localhost:3001/updateQuantity/${bookId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            quantity: newQuantity,
+        }),
+    });
+  
+    if (response.ok) {
+  
+        const bookSubtitle = document.querySelector(`.card-subtitle[data-book-id="${bookId}"]`);
+        bookSubtitle.textContent = `Available: ${newQuantity}`;
+    } else {
+        console.error('Failed to update quantity');
+    }
+  }
+  
+  main()
